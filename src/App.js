24bf3b4f3ccc6f24b5885ecc5c9c99fe29689b47
JoseLAmador <josel.amador.s@gmail.com>
2018-03-06 22:55:54 -0600

    :trollface:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div >
-          <div>
+      <div style={{height:'100vh', display:'flex', backgroundColor:'red'}}>
+          <div style={{width:'20%', display:'flex', backgroundColor:'green'}}>
               another
           </div>
-        <main>
-           <section className="left">
-               <div className="left-profile">
-                   some text here
-               </div>
-
+        <main style={{display:'flex', backgroundColor:'aliceblue', width:'80%', flexDirection:'column', margin:'2%'}}>
+           <section style={{display:'flex', height:'33.33%', backgroundColor:'orange'}}>
+               some text here
            </section>
 
-           <section className="right">
-               <div>one div</div>
-               <div>two div</div>
+           <section style={{display:'flex', height:'33.33%', backgroundColor:'pink'}}>
+               card 2
            </section>
         </main>
       </div>
